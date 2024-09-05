3998fe8f246f710f8a18d1ca4a7f08de3faffe4e
min20ta <127599615+min20ta@users.noreply.github.com>
2024-09-05 16:00:20 +0900

    Update RetrofitInstance.kt

--- app/src/main/java/com/example/hometab/RetrofitInstance.kt
+++ app/src/main/java/com/example/hometab/RetrofitInstance.kt
@@ -12,22 +12,16 @@ object RetrofitInstance{
 
         private const val BASE_URL = "http://10.0.2.2:3000/"
 
-
-
-
-
-    var builder = OkHttpClient().newBuilder()
-    var okHttpClient = builder
-        .connectTimeout(1000, TimeUnit.SECONDS)   //소켓타임아웃방지
-        .readTimeout(1000,TimeUnit.SECONDS)
-        .writeTimeout(1000, TimeUnit.SECONDS)
-        .followRedirects(false)
-        .build()
-
-
-
+        var builder = OkHttpClient().newBuilder()
+        var okHttpClient = builder
+                .connectTimeout(1000, TimeUnit.SECONDS)   //소켓타임아웃방지
+                .readTimeout(1000,TimeUnit.SECONDS)
+                .writeTimeout(1000, TimeUnit.SECONDS)
+                .followRedirects(false)
+                .build()
+        
         var gson=GsonBuilder().setLenient().create()
-
+        
         val client = Retrofit
             .Builder()
             .baseUrl(BASE_URL)
@@ -39,37 +33,6 @@ object RetrofitInstance{
         fun getInstance() : Retrofit {
             return client
         }
-
-
-//    private var instance: Retrofit? = null
-//    private val gson = GsonBuilder().setLenient().create()
-//
-//
-//    // timeout시간을 설정해줍니다.
-//    private const val CONNECT_TIMEOUT_SEC = 20000L
-//
-//    fun getInstance(): Retrofit {
-//        if(instance == null) {
-//            val interceptor = HttpLoggingInterceptor()
-//            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
-//
-//            val client = OkHttpClient.Builder()
-//                .cookieJar(JavaNetCookieJar(CookieManager()))
-//                .addInterceptor(interceptor)
-//                .connectTimeout(CONNECT_TIMEOUT_SEC, TimeUnit.SECONDS)
-//                .build()
-//
-//            instance = Retrofit.Builder()
-//                .baseUrl(BASE_URL)
-////                .addConverterFactory(ScalarsConverterFactory.create())
-//                .addConverterFactory(GsonConverterFactory.create(gson))
-//                .addCallAdapterFactory(CoroutineCallAdapterFactory())
-//                .client(client)
-//                .build()
-//        }
-//
-//        return instance!!
-//    }
 }
 
 
